2e7518016ecdb01d9ab41da07a6716173cdd0f95
MuhsinKamil <muhsinwahab@gmail.com>
2021-01-26 22:23:05 +0530

    Adding templates using handlebars
    setting index, about, help with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,41 @@
-const path = require('path')
+const path = require("path")
 const express = require("express")
 
 const app = express()
-const publicFolder = path.join(__dirname, '../public')
+const publicFolder = path.join(__dirname, "../public")
+const viewsFolder = path.join(__dirname, "../templates")
 
-// goes to the public folder and serves the files
+// set up for rendering static files
 app.use(express.static(publicFolder))
 
-app.get("/weather" ,(req, res) => {
+// set handlebar for Dynamic rendering
+app.set("view engine", "hbs")
+app.set("views", viewsFolder)
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Mohamed Muhsin",
+  })
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About page",
+  })
+})
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    message: "Information by Help page",
+  })
+})
+
+app.get("/weather", (req, res) => {
   res.send({
     forecast: "cloudy",
-    location: "London"
+    location: "London",
   })
 })
 
