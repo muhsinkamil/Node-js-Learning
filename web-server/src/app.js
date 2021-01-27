401883fb2433484116664ea282137875e10b6dd5
MuhsinKamil <muhsinwahab@gmail.com>
2021-01-27 11:24:06 +0530

    Adding templates  using hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,11 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
 
 const app = express()
 const publicFolder = path.join(__dirname, "../public")
-const viewsFolder = path.join(__dirname, "../templates")
+const viewsFolder = path.join(__dirname, "../templates/views")
+const partialFolder = path.join(__dirname, "../templates/partials")
 
 // set up for rendering static files
 app.use(express.static(publicFolder))
@@ -11,6 +13,7 @@ app.use(express.static(publicFolder))
 // set handlebar for Dynamic rendering
 app.set("view engine", "hbs")
 app.set("views", viewsFolder)
+hbs.registerPartials(partialFolder)
 
 app.get("", (req, res) => {
   res.render("index", {
