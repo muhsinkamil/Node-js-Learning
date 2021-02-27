1fbfdcea456b5328e86a77b1a15a1edc36e1b877
MuhsinKamil <muhsinwahab@gmail.com>
2021-02-27 22:44:04 +0530

    Initialising multer and using local dest folder to upload

--- TaskManager/src/index.js
+++ TaskManager/src/index.js
@@ -8,6 +8,20 @@ const taskRouter = require("./Router/taskRouter")
 
 const app = express()
 
+// const multer = require("multer")
+// const upload = multer({ dest: "avatar" })
+
+// app.post("/avatar", upload.single("images"), (req, res) => {
+//   res.send()
+// })
+
+const multer = require("multer")
+const upload = multer({ dest: "avatars" })
+
+app.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
+  res.send()
+})
+
 app.use(express.json())
 app.use("/users", userRouter)
 app.use("/tasks", taskRouter)
